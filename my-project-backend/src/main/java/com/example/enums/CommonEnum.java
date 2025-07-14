55d080080be73bf41637c7b079de822f85d7d8b8
江彦 杨 <yangjiangyan@cnstrong.cn>
2025-07-14 22:05:29 +0800

    refactor(my-project-backend): 修改 LabelEnums 中可培训的排序顺序- 移除 LabelEnums 中的 Threshold_Low、Easy_Money 和 Remote 选项 - 调整 Can_Train 的排序顺序，将其移动到第 3 位

--- my-project-backend/src/main/java/com/example/enums/CommonEnum.java
+++ my-project-backend/src/main/java/com/example/enums/CommonEnum.java
@@ -16,10 +16,7 @@ public class CommonEnum {
     public enum LabelEnums {
         Long_Term_Cooperation(1, "长期合作"),
         Suitable_For_New_Users(2, "适合新手"),
-        Threshold_Low(3, "零门槛"),
-        Can_Train(4, "可培训"),
-        Easy_Money(5, "轻松赚钱"),
-        Remote(5, "可远程"),
+        Can_Train(3, "可培训"),
 
         ;
         private Integer code;
