94510f69c45c4d05b92697cd7a376d3095b8cad6
江彦 杨 <yangjiangyan@cnstrong.cn>
2025-11-26 22:33:11 +0800

    feat(account): 临时绕过短信服务并硬编码验证码

--- my-project-backend/src/main/java/com/example/service/impl/AccountServiceImpl.java
+++ my-project-backend/src/main/java/com/example/service/impl/AccountServiceImpl.java
@@ -18,7 +18,6 @@ import com.example.service.RedisService;
 import com.example.utils.Const;
 import com.example.utils.DateUtils;
 import com.example.utils.FlowUtils;
-import com.example.utils.SmsUtils;
 import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -212,9 +211,15 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
         SendSmsResponse response = null;
         try {
             //TODO yang open
-            response = SmsUtils.sendSms(phone, templateParams);
-            log.info("发送短信，phone:{},code:{},resp:{}",phone, code, JSON.toJSONString(response));
-            redisService.saveValue(Const.VERIFY_PHONE_DATA + phone, String.valueOf(code), 5, TimeUnit.MINUTES);
+            //response = SmsUtils.sendSms(phone, templateParams);
+
+            if(phone.equals("15569640815")){
+                log.info("发送短信，phone:{},code:{},resp:{}",phone, "111111", JSON.toJSONString(response));
+                redisService.saveValue(Const.VERIFY_PHONE_DATA + phone, String.valueOf("111111"), 5, TimeUnit.MINUTES);
+            } else {
+                log.info("发送短信，phone:{},code:{},resp:{}",phone, code, JSON.toJSONString(response));
+                redisService.saveValue(Const.VERIFY_PHONE_DATA + phone, String.valueOf(code), 5, TimeUnit.MINUTES);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
